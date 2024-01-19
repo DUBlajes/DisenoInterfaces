2d51c66591a8ebe86d0dded842840aa9e73dd41a
Álvaro Guerrero Sánchez <dublajes@gmail.com>
2024-01-19 12:21:19 +0100

    Subida ejercicio 5

--- Segundo Trimestre/segundoTrimestre/src/module-info.java
+++ Segundo Trimestre/segundoTrimestre/src/module-info.java
@@ -8,4 +8,8 @@ module tema5.DiseñoDeComponentes {
 	opens tema5.Ejercicio2 to javafx.graphics, javafx.fxml;
 	opens tema5.Ejercicio3 to javafx.graphics, javafx.fxml;
 	opens tema5.Ejercicio4 to javafx.graphics, javafx.fxml;
+	opens tema5.Ejercicio6 to javafx.graphics, javafx.fxml;
+	opens tema5.Ejercicio7 to javafx.graphics, javafx.fxml;
+	opens tema5.Ejercicio9 to javafx.graphics, javafx.fxml;
+	opens tema5.Ejercicio10 to javafx.graphics, javafx.fxml;
 }
